62872aea2c837550ea43a1be6805749520683c48
Alana-Hague <122403099+Alana-Hague@users.noreply.github.com>
2023-01-25 12:16:51 -0500

    Add files via upload

---
+++ fasta_seq_count.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+#task_1
+echo "This script can count the number of sequences in a fasta file."
+
+greeting="hello"
+name="Alana"
+
+echo $greeting $name
+
+#task_2
+#directories have been created 
+
+#task_3
+#cp transcriptome.fasta /Users/alanahague/BIOS_5970/assignment_2/learning_bash/bioinformatics
+#this command assumes that you are in the same folder as transcriptome.fasta
+
+#task_4
+seq=$(grep ">" ./learning_bash/bioinformatics/transcriptome.fasta | wc -l)
+echo "the number of sequences is" $seq > ./learning_bash/bioinformatics/fasta_count.txt
+
