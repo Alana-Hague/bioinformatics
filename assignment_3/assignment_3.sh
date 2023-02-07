b7ef0de284167d780ab45929987c5383e1be4d1a
Alana-Hague <122403099+Alana-Hague@users.noreply.github.com>
2023-02-06 23:07:00 -0500

    Add files via upload

---
+++ assignment_3/assignment_3.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Task 1
+sed 's/Zoo//' ./pandas.txt > ./task1.txt
+
+# Task 2
+awk '{print $1,$3,$2}' ./pandas.txt > ./temp.txt && sort -k3 ./temp.txt > ./task2.txt
+
+# Task 3
+awk '{print $5}' ./pandas.txt > temp.txt && sort -k1 ./temp.txt > ./task3.txt
+
+# Task 4
+sed 's/female/F/g' ./pandas.txt > temp.txt && sed 's/male/M/g' ./temp.txt > ./task4.txt
+
+# Task 5
+grep -c "October" ./pandas.txt
+
+# Task 6
+grep -n "a" ./pandas.txt > ./task6.txt  # using sed would be a better command here, as shown below
+#sed -e 's/^/prefix/' ./pandas.txt > ./task6.txt
+
+# Task 7
+sed -n 1p ./pandas.txt > ./task7.txt && 
+sed -n 6p ./pandas.txt >> ./task7.txt && 
+sed -n 17p ./pandas.txt >> ./task7.txt
+
+# Task 8
+grep -h "X" ./pandas.txt > ./task8.txt && grep -h "x" ./pandas.txt >> ./task8.txt
+
+# Task 9
+awk '{print $5}' ./pandas.txt > ./temp.txt && uniq -u ./temp.txt > ./task9.txt
+
+# Task 10
+awk '$3>2013{print $1, $3}' ./pandas.txt > ./temp.txt &&
+sort -k 2n ./temp.txt > ./task10.txt
