ae706827a99d4f146886ec13ba928eb171852269
Alana-Hague <122403099+Alana-Hague@users.noreply.github.com>
2023-02-15 23:45:31 -0500

    Add files via upload

--- assignment4.sh
+++ assignment4.sh
@@ -14,6 +14,7 @@ awk -F':' '{printf "%s%s", $5, (/^Grade/)?"\n":"\t|"}' mystery_transcripts.fasta
 # Task 4
 blastn -query ./mystery_nucleotide_query.fasta mystery_transcripts.fasta &&
 blastn -c -evalue 1e-3 mystery_transcripts.fasta 
+T010027234997
 
 # Task 5
 awk -c 1200 ./mystery_nucleotide_query.fasta | blastn -query ./mystery_nucleotide_query.fasta mystery_transcripts.fasta > most_similar.txt
@@ -26,14 +27,15 @@ awk -F':' '{printf "%s%s", $15, (/^Grade/)?"\n":"\t|"}' mystery_protiens.fasta
 # Task 7
 blastn -query ./mystery_protien_query.fasta mystery_protiens.fasta &&
 echo blastn -c -evalue 1e-5 mystery_protiens.fasta 
+P010027090796
 
 # Task 8
-# Coffea Arabica
+Coffea Arabica
 
 # Task 9 
-# Protien Query  = Cicer arietinum (chickpea)
+Gardenia Jasmineoides
 
 # Task 10
-#chickpeas have NBS-LRR genes
+Putative Catalase
 
 
