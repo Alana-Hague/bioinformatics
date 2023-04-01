738f8db6fb2a687b3ad38a2afd28b3044457b0b2
Alana-Hague <122403099+Alana-Hague@users.noreply.github.com>
2023-04-01 13:57:49 -0400

    Add files via upload

--- assignment_3/assignment_3.sh
+++ assignment_3/assignment_3.sh
@@ -4,7 +4,7 @@
 sed 's/Zoo//' ./pandas.txt > ./task1.txt
 
 # Task 2
-awk '{print $1,$3,$2}' ./pandas.txt > ./temp.txt && sort -k3 ./temp.txt > ./task2.txt
+awk '{print $1,$4,$2}' ./pandas.txt > ./temp.txt && sort -k3 ./temp.txt > ./task2.txt
 
 # Task 3
 awk '{print $5}' ./pandas.txt > temp.txt && sort -k1 ./temp.txt > ./task3.txt
@@ -13,7 +13,8 @@ awk '{print $5}' ./pandas.txt > temp.txt && sort -k1 ./temp.txt > ./task3.txt
 sed 's/female/F/g' ./pandas.txt > temp.txt && sed 's/male/M/g' ./temp.txt > ./task4.txt
 
 # Task 5
-grep -c "October" ./pandas.txt
+grep -c "October" ./pandas.txt > ./task5.txt
+# ANSWER IS 2; output to a new file anyway
 
 # Task 6
 grep -n "a" ./pandas.txt > ./task6.txt  # using sed would be a better command here, as shown below
@@ -31,5 +32,6 @@ grep -h "X" ./pandas.txt > ./task8.txt && grep -h "x" ./pandas.txt >> ./task8.tx
 awk '{print $5}' ./pandas.txt > ./temp.txt && uniq -u ./temp.txt > ./task9.txt
 
 # Task 10
-awk '$3>2013{print $1, $3}' ./pandas.txt > ./temp.txt &&
-sort -k 2n ./temp.txt > ./task10.txt
+#awk '$4>2013{print $1, $4}' ./pandas.txt > ./temp.txt &&
+#sort -k 2n ./temp.txt > ./task10.txt
+sort -k4 -n pandas.txt | awk '$4 <= 10' | awk '{print $1, $4}' > ./task10.txt
